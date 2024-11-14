21cf70c4d955c65aac7b8d544ab902c5be941572
Ahmed Samy <marcheder200.200@gmail.com>
2024-11-14 21:05:59 +0200

    Creating Swapchain

--- Vulkan/main.cpp
+++ Vulkan/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -8,33 +10,177 @@
 #define WINDOW_HEIGHT 720
 
 GLFWwindow* window = NULL;
+VkInstance instance;
+VkSurfaceKHR surface;
+VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+VkDevice device;
+VkQueue graphicsQueue;
+VkSwapchainKHR swapchain;
 
-void GLFW_KeyCallback(GLFWwindow* winodw, int key, int scancode, int action, int mods)
-{
-	if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS))
-	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
+
+void GLFW_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS)) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
 }
 
-int main(int argc, char* argv[])
-{
-	if (!glfwInit()) {return 1;}
-	if (!glfwVulkanSupported()) { return 1; }
+void createInstance() {
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "Bimo";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_2;
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", NULL, NULL);
-	if (!window) { glfwTerminate(); exit(EXIT_FAILURE); }
-	glfwSetKeyCallback(window, GLFW_KeyCallback);
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
 
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance!\n";
+        exit(EXIT_FAILURE);
+    }
+}
 
-	while(!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-	}
+uint32_t findQueueFamilies(VkPhysicalDevice device) {
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
 
-	glfwTerminate();
-	return 0;
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    for (uint32_t i = 0; i < queueFamilyCount; i++) {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+            if (presentSupport) {
+                return i;
+            }
+        }
+    }
+
+    throw std::runtime_error("Failed to find a suitable queue family!");
+}
+
+void choosePhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0) {
+        std::cerr << "Failed to find GPUs with Vulkan support!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    physicalDevice = devices[0];
+    if (physicalDevice == VK_NULL_HANDLE) {
+        std::cerr << "Failed to find a suitable physical device!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void createDeviceAndQueues() {
+    uint32_t queueFamilyIndex = findQueueFamilies(physicalDevice);
+
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = queueFamilyIndex;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    const char* deviceExtensions[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+    VkDeviceCreateInfo deviceCreateInfo = {};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
+    deviceCreateInfo.queueCreateInfoCount = 1;
+    deviceCreateInfo.enabledExtensionCount = 1;
+    deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions;
+
+    if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) {
+        std::cerr << "Failed to create logical device!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    vkGetDeviceQueue(device, queueFamilyIndex, 0, &graphicsQueue);
+}
+
+void createSwapChain() {
+    VkSurfaceCapabilitiesKHR capabilities;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &capabilities);
+
+    VkSurfaceFormatKHR surfaceFormat = {};
+    surfaceFormat.format = VK_FORMAT_B8G8R8A8_UNORM;
+    surfaceFormat.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
+
+    VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
+    swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    swapchainCreateInfo.surface = surface;
+    swapchainCreateInfo.minImageCount = capabilities.minImageCount + 1;
+    swapchainCreateInfo.imageFormat = surfaceFormat.format;
+    swapchainCreateInfo.imageColorSpace = surfaceFormat.colorSpace;
+    swapchainCreateInfo.imageExtent = capabilities.currentExtent;
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+    swapchainCreateInfo.preTransform = capabilities.currentTransform;
+    swapchainCreateInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    swapchainCreateInfo.presentMode = VK_PRESENT_MODE_FIFO_KHR;
+    swapchainCreateInfo.clipped = VK_TRUE;
+
+    uint32_t queueFamilyIndex = findQueueFamilies(physicalDevice);
+    swapchainCreateInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    swapchainCreateInfo.queueFamilyIndexCount = 1;
+    swapchainCreateInfo.pQueueFamilyIndices = &queueFamilyIndex;
+
+    if (vkCreateSwapchainKHR(device, &swapchainCreateInfo, nullptr, &swapchain) != VK_SUCCESS) {
+        std::cerr << "Failed to create swapchain!\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+void initVulkan() {
+    createInstance();
+    if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {std::cerr << "Failed to create window surface!\n";exit(EXIT_FAILURE);}
+    choosePhysicalDevice();
+    createDeviceAndQueues();
+    createSwapChain();
+}
+
+int main(int argc, char* argv[]) {
+    if (!glfwInit()) { return 1; }
+    if (!glfwVulkanSupported()) { return 1; }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwSetKeyCallback(window, GLFW_KeyCallback);
+
+    initVulkan();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+    vkDestroyDevice(device, nullptr);
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+    vkDestroyInstance(instance, nullptr);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
